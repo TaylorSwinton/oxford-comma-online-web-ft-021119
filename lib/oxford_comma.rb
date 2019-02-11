859cfdbf3d2d78ff42aac8e5173314a677996f6e
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-11 22:07:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
       array.join(" and ")
     elsif array.length == 3
       array.each do |fruit|
-        newArray.join ("#{fruit}, #{fruit}, and #{fruit}")
+        array.join ("#{fruit}, #{fruit}, and #{fruit}")
       end
     end
 end
