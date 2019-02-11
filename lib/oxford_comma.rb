1486112cebc5767f073ecde70ce008b13c6f9b81
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-11 22:11:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,7 @@
 def oxford_comma(array)
-    if array.length == 1
-      array.join
-    elsif array.length == 2
-      array.join(" and ")
-    elsif array.length == 3
-      array.each do |fruit|
-        array.join puts ("#{fruit}, #{fruit}, and #{fruit}")
-      end
-    end
+  unless array.length >= 3
+
+  else
+    array.join(" and ")
+  end
 end
