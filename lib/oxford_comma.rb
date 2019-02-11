03557bbadcd7c9c95d7d73822c15b2bb668c76b7
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-11 22:44:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   else array.length >= 3
-    array[-1].insert(-6, ' and ')
+    array[-1].insert(-11, ' and ')
     array.join(", ")
   end
 end
